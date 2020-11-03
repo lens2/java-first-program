925bce9c4006a3691d178dd8b7d3a7b91859279c
lens2 <73800729+lens2@users.noreply.github.com>
2020-11-03 12:43:17 +0100

    MortageCalculator Solution

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -1,14 +1,33 @@
 package com.h2;
 
-import javax.swing.*;
-
 public class MortgageCalculator {
     private long loanAmount;
-    private int termInYear;
+    private int termInYears;
     private float annualRate;
     private double monthlyPayment;
 
+    public MortgageCalculator(long loanAmount, int termInYears, float annualRate){
+        this.loanAmount = loanAmount;
+        this.termInYears = termInYears;
+        this.annualRate = annualRate;
+    }
 
+    private int getNumberOfPayments(){
+        return termInYears * 12;
+    }
+
+    private float getMonthlyInterestRate(){
+        float interestRate = annualRate / 100;
+        return interestRate / 12;
+    }
+
+    public void calculateMonthlyPayment(){
+        long P = loanAmount;
+        float r = getMonthlyInterestRate();
+        int n = getNumberOfPayments();
+        double M = P * (((r * Math.pow(1 + r, n))) / ((Math.pow((1 + r), n)) - 1));
+        this.monthlyPayment = M;
+    }
 
 
 }
