e6ca88170ce5f70ec7e5b6d95ccc94ef0e7bd228
lens2 <73800729+lens2@users.noreply.github.com>
2020-11-03 12:08:48 +0100

    MortageCalculator Solution

---
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -0,0 +1,14 @@
+package com.h2;
+
+import javax.swing.*;
+
+public class MortgageCalculator {
+    private long loanAmount;
+    private int termInYear;
+    private float annualRate;
+    private double monthlyPayment;
+
+
+
+
+}
