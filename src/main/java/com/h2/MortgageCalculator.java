3c0a67676e46c172d0d1d91549ba9c11e4514b2a
lens2 <73800729+lens2@users.noreply.github.com>
2020-11-03 12:49:03 +0100

    MortageCalculator Solution

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -1,5 +1,7 @@
 package com.h2;
 
+import java.text.DecimalFormat;
+
 public class MortgageCalculator {
     private long loanAmount;
     private int termInYears;
@@ -27,6 +29,11 @@ public class MortgageCalculator {
         int n = getNumberOfPayments();
         double M = P * (((r * Math.pow(1 + r, n))) / ((Math.pow((1 + r), n)) - 1));
         this.monthlyPayment = M;
+    }
+
+    public String toString(){
+        DecimalFormat df = new DecimalFormat("####0.00");
+        return "monthlyPayment : " + df.format(monthlyPayment);
     }
 
 
