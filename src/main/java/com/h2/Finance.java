389f5c3f3cf663a989d4416dd776845a2a32a835
lens2 <73800729+lens2@users.noreply.github.com>
2020-11-03 13:10:46 +0100

    MortageCalculator Solution

--- src/main/java/com/h2/Finance.java
+++ src/main/java/com/h2/Finance.java
@@ -1,13 +1,6 @@
 package com.h2;
 
 public class Finance {
-    public final static (){
-       String BEST_LOAN_RATE = "bestLoanRates";
-       String SAVINGS_CALCULATOR = "savingsCalculator";
-       String MORTGAGE_CALCULATOR = "mortgageCalculator";
-    }
 
-    public final static commandsToUsage(){
 
-    }
 }
